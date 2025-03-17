1a657bba554c750c3fbc52b14326ea56db6294d6
FrozenFOXX <frozenfoxx@cultoffoxx.net>
2025-03-17 12:50:31 -0700

    Updated installs

--- addons.sh
+++ addons.sh
@@ -5,26 +5,6 @@ LC_ALL="C"
 
 # Functions
 
-## Check shell
-check_shell()
-{
-  if [[ ! $0 = "-zsh" ]]; then
-    echo "Shell: $(echo $0 | tr -d '-')"
-
-    # Check if zsh is installed
-    if [[ -f $(which zsh) ]]; then
-      echo "Changing default shell to zsh, input user password:"
-      chsh -s $(which zsh)
-      . ~/.zshrc
-    else
-      echo "zsh is not installed. Please install it first, set it to default, and relogin"
-      exit 1
-    fi
-  else
-    echo "Shell: zsh"
-  fi
-}
-
 ## Check if a function exists
 fn_exists()
 {
@@ -43,25 +23,13 @@ install_homebrew()
   fi
 }
 
-## Install Oh-My-Zsh
-install_omz()
-{
-  if ! fn_exists omz &> /dev/null; then
-    echo "Installing oh-my-zsh..."
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-  else
-    echo "Updating oh-my-zsh..."
-    omz update
-  fi
-}
-
 ## Install PyEnv
 install_pyenv()
 {
   if ! fn_exists pyenv &> /dev/null; then
     echo "Installing PyEnv..."
     curl -fsSL https://pyenv.run | bash
-    git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+    #git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
   fi
 }
 
@@ -72,6 +40,4 @@ if [[ $(uname -s) == 'Darwin' ]]; then
   install_homebrew
 fi
 
-check_shell
-install_omz
 install_pyenv
