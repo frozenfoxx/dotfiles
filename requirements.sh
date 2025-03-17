6d2ea9666f0440487baa453eb90711d927f7f285
FrozenFOXX <frozenfoxx@cultoffoxx.net>
2025-03-17 12:05:21 -0700

    Updated requirements script

--- requirements.sh
+++ requirements.sh
@@ -1,9 +1,38 @@
 #!/usr/bin/env bash
 
-# macOS Specific
-if [[ $(uname -s) == 'Darwin' ]]; then
+# Variables
+LC_ALL="C"
 
-  # Install Homebrew
+# Functions
+
+## Change shell
+change_shell()
+{
+  if [[ ! $0 = "-zsh" ]]; then
+    echo "Changing default shell to zsh..."
+
+    # Set shell to zsh if installed or error out
+    if [[ -f $(which zsh) ]]; then
+      chsh -s $(which zsh)
+      . ~/.zshrc
+    else
+      echo "zsh is not installed, please install it first!"
+      exit 1
+    fi
+  else
+    echo 'Default shell is zsh'
+  fi
+}
+
+## Check if a function exists
+fn_exists()
+{
+  type $1 | grep -q 'shell function'
+}
+
+## Install Homebrew (macOS)
+install_homebrew()
+{
   if ! command -v brew &> /dev/null; then
     echo "Installing homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -11,28 +40,37 @@ if [[ $(uname -s) == 'Darwin' ]]; then
     echo "Updating homebrew..."
     brew update
   fi
+}
+
+## Install Oh-My-Zsh
+install_omz()
+{
+  if ! fn_exists(omz) &> /dev/null; then
+    echo "Installing oh-my-zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  else
+    echo "Updating oh-my-zsh..."
+    omz update
+  fi
+}
+
+## Install PyEnv
+install_pyenv()
+{
+  if ! fn_exists(pyenv) &> /dev/null; then
+    echo "Installing PyEnv..."
+    curl -fsSL https://pyenv.run | bash
+    git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+  fi
+}
+
+# Logic
+
+## macOS Specific
+if [[ $(uname -s) == 'Darwin' ]]; then
+  install_homebrew
 fi
 
-# Install oh-my-zsh
-if ! command -v omz &> /dev/null; then
-  echo "Installing oh-my-zsh..."
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-else
-  echo "Updating oh-my-zsh..."
-  omz update
-fi
-
-# Install PyEnv
-if ! command -v pyenv &> /dev/null; then
-  echo "Installing PyEnv..."
-  curl -fsSL https://pyenv.run | bash
-  git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
-fi
-
-# Change default shell
-if [[ ! $0 = "-zsh" ]]; then
-  echo "Changing default shell to zsh..."
-  chsh -s /bin/zsh
-else
-  echo 'Already using zsh'
-fi
+change_shell
+install_omz
+install_pyenv
